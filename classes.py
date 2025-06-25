0f156f9cfa149fcb8eea89a0baba7f26508e1b6d
soilad <joshtgm@icloud.com>
2025-06-25 16:21:59 +0530

    added centered text

--- classes.py
+++ classes.py
@@ -9,6 +9,8 @@ font = font.Font(f"{cwd}/ui/font.ttf", 24)
 
 
 class Button:
+    mouse_up = False
+
     def __init__(self, id, pos, dim, thicc):
         self.id = id
         self.pos = pos
@@ -23,6 +25,14 @@ class Button:
             self.dim[0],
             self.dim[1],
         )
+
+    def check(self, status, mpos):
+        if Rect.collidepoint(self.brect, mpos) and status == "up":
+            self.mouse_up = True
+        if Rect.collidepoint(self.brect, mpos) and status == "down" and self.mouse_up:
+            print(self.id)
+        if status == "down":
+            self.mouse_up = False
 
     def draw(self, screen, mpos):
         if Rect.collidepoint(self.brect, mpos):
@@ -67,11 +77,12 @@ class Button:
             (self.pos[0] + (self.dim[0] >> 1), self.pos[1] + (self.dim[1] >> 1)),
             (self.dim[1] >> 1),
         )
+        font_render = font.render(
+            self.id,
+            fontalias,
+            primary if Rect.collidepoint(self.brect, mpos) else secondary,
+        )
         screen.blit(
-            font.render(
-                self.id,
-                fontalias,
-                primary if Rect.collidepoint(self.brect, mpos) else secondary,
-            ),
-            (self.pos[0] - (self.dim[0] >> 1), self.pos[1] + self.thicc),
+            font_render,
+            (self.pos[0] - (font_render.get_width() >> 1), self.pos[1] + self.thicc),
         )
--- main.py
+++ main.py
@@ -9,7 +9,11 @@ menu = True
 # themeing shit
 menubg = pygame.image.load(f"{cwd}/ui/menu.png")
 
-startb = Button("Start Game", (100, 100), (120, 40), 5)
+startb = Button("Start Game", (200, 400), (160, 40), 5)
+continueb = Button("Continue Game", (200, 500), (160, 40), 5)
+settingsb = Button("Settings", (200, 600), (160, 40), 5)
+exitb = Button("Exit", (200, 700), (160, 40), 5)
+
 screen = pygame.display.set_mode(
     (1920, 1080), pygame.FULLSCREEN | pygame.DOUBLEBUF | pygame.SCALED, vsync=1
 )
@@ -20,7 +24,20 @@ while run:
         match event.type:
             case pygame.QUIT:
                 run = False
+            case pygame.MOUSEBUTTONDOWN:
+                startb.check("up", mpos)
+                continueb.check("up", mpos)
+                settingsb.check("up", mpos)
+                exitb.check("up", mpos)
+            case pygame.MOUSEBUTTONUP:
+                startb.check("down", mpos)
+                continueb.check("down", mpos)
+                settingsb.check("down", mpos)
+                exitb.check("down", mpos)
     if menu:
         startb.draw(screen, mpos)
+        continueb.draw(screen, mpos)
+        settingsb.draw(screen, mpos)
+        exitb.draw(screen, mpos)
 
     pygame.display.update()
